6e8c431530443735d9b7d1c49b11240258aeb269
it-logisfloreal <it.epicerie@logisfloreal.be>
2016-12-24 11:09:30 +0100

    Create __openerp__.py
    Initial commit

---
+++ logisfloo/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Logis-Floréal Base Module",
+
+    'summary': """
+		Module that customize the Beesdoo base module and contains some python tools
+     """,
+
+    'description': """
+    """,
+
+    'author': "Logis-Floréal - Cellule IT",
+    'website': "https://github.com/logisfloo",
+
+    'category': 'Project Management',
+    'version': '0.1',
+
+    'depends': ['Obeesdoo', 'point_of_sale', 'purchase'],
+
+    'data': [
+    ],
+}
