5dd3884ce3de8d8c0fce12d0fec1abc91bfebec6
Guy Raspe <guy.raspe@gmail.com>
2017-07-30 21:03:12 +0200

    Added field in product view to automatically compute price with margin
    The price with margin is the purchase price + 5%.
    Margin is had coded should be a configurable parameter to define
    margin.

--- logisfloo_base/models/product.py
+++ logisfloo_base/models/product.py
@@ -1,13 +1,12 @@
  # -*- coding: utf-8 -*-
 from openerp import models, fields, api
 
-class Prodcut(models.Model):
+class Product(models.Model):
 
     _inherit = 'product.template'
     
-    total_with_margin = fields.Float(compute='_get_total_with_margin', store=True, string="Total Sales Price with Margin")
+    total_with_margin = fields.Float(compute='_get_total_with_margin', store=False, string="Total Sales Price with Margin")
     
     def _get_total_with_margin(self):
-        margin_amount = self.list_price * 0.05
-        self.total_with_vat = self.list_price + margin_amount
-    
+        margin_amount = self.standard_price * 0.05
+        self.total_with_margin = self.standard_price + margin_amount
