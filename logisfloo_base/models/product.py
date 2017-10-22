ddaf30c5c1899cf566579073ed0e06617f6853a6
Guy Raspé <guy.raspe@gmail.com>
2017-10-22 23:15:14 +0200

    Add reference_price field in product view to track reference price on invoice. This allows tracking change in product cost independently of the VAT and discount (used as a manual control field).

--- logisfloo_base/models/product.py
+++ logisfloo_base/models/product.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     _inherit = 'product.template'
     
     total_with_margin = fields.Float(compute='_get_total_with_margin', store=False, string="Total Sales Price with Margin")
+    reference_price = fields.Float(string='Invoice Price')
     producer = fields.Char(string="Producer")
     
     def _get_total_with_margin(self):
