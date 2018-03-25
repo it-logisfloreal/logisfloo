08abc11edcce0bd21112fe7efbf4558b23d5112d
Guy Raspé <guy.raspe@gmail.com>
2018-03-25 13:44:08 +0200

    Clean up override functions handling the product discount.
    _onchange_quantity should call super and not copy the existing code
    @api.onchange is not required when overriding
    @api.model is not required  when overriding create method

--- logisfloo_base/models/purchase.py
+++ logisfloo_base/models/purchase.py
@@ -17,52 +17,31 @@ class LogisflooPurchaseOrderLine(models.Model):
                 'price_total': taxes['total_included'],
                 'price_subtotal': taxes['total_excluded'],
             })
-    
-    @api.onchange('product_qty', 'product_uom')
-    def _onchange_quantity(self):
-        if not self.product_id:
-            return
 
+    def _get_seller_discount(self):
+        if not self.product_id:
+            return 
         seller = self.product_id._select_seller(
             self.product_id,
             partner_id=self.partner_id,
             quantity=self.product_qty,
             date=self.order_id.date_order and self.order_id.date_order[:10],
             uom_id=self.product_uom)
-
-        if seller or not self.date_planned:
-            self.date_planned = self._get_date_planned(seller).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-
         if not seller:
-            return
-
-        price_unit = self.env['account.tax']._fix_tax_included_price(seller.price, self.product_id.supplier_taxes_id, self.taxes_id) if seller else 0.0
-        if price_unit and seller and self.order_id.currency_id and seller.currency_id != self.order_id.currency_id:
-            price_unit = seller.currency_id.compute(price_unit, self.order_id.currency_id)
-
-        if seller and self.product_uom and seller.product_uom != self.product_uom:
-            price_unit = self.env['product.uom']._compute_price(seller.product_uom.id, price_unit, to_uom_id=self.product_uom.id)
-
-        self.price_unit = price_unit
-        self.discount = seller.discount
+            return 
+        return seller.discount
+    
+    # Override the PO line onchange quantity method to update the seller discount on the PO line
+    def _onchange_quantity(self):
+        super(LogisflooPurchaseOrderLine,self)._onchange_quantity()
+        self.discount = self._get_seller_discount()
 
     # Override the PO line create method to add the seller discount on the PO line
-    @api.model
+    #@api.model
     def create(self, values):
         record = super(LogisflooPurchaseOrderLine,self).create(values)
-        if not record.product_id:
-            return record
-        seller = record.product_id._select_seller(
-            record.product_id,
-            partner_id=record.partner_id,
-            quantity=record.product_qty,
-            date=record.order_id.date_order and record.order_id.date_order[:10],
-            uom_id=record.product_uom)
-        if not seller:
-            return record
-        record['discount'] = seller.discount
+        record['discount'] = record._get_seller_discount()
         return record
-
     
 class LogisflooAccountInvoice(models.Model):
     _inherit = 'account.invoice'
