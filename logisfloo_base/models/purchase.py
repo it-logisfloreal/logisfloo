dfb90e0272793819953cccad7aaf47cc571b407a
Guy Raspé <guy.raspe@gmail.com>
2018-07-27 19:45:54 +0200

    Handle invoice payment by third party (kind of expense).
    The third party payer (a known contact) must be defined on the invoice.
    The invoice remains linked to the Vendor. 
    When registering the payment, the Vendor invoice is marked as paid with
    a matching move on an interim third party payment account using a thrid
    party payment journal. 
    The actual liquidity payment is immediately completed by a matching move
    from this interim account to the liquidity account using the
    corresponding liquidity journal.

--- logisfloo_base/models/purchase.py
+++ logisfloo_base/models/purchase.py
@@ -75,6 +75,10 @@ class LogisflooPurchaseOrderLine(models.Model):
 class LogisflooAccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
+    tpty_partner_id = fields.Many2one('res.partner', string='ThirdParty Partner', change_default=True,
+        required=False, readonly=True, states={'draft': [('readonly', False)], 'open': [('readonly', False)]},
+        track_visibility='always')    
+
     def _prepare_invoice_line_from_po_line(self, line):
         data=super(LogisflooAccountInvoice,self)._prepare_invoice_line_from_po_line(line)
         data['discount'] = line.discount or 0.0
@@ -187,5 +191,107 @@ class LogisflooAdjustInvoiceWizard(models.TransientModel):
                                           readonly=True)
     InvoicedTotalAmount = fields.Monetary(string='Amount on invoice', currency_field='company_currency_id')
     AdjustmentAmount = fields.Monetary(string='Adjustment amount', currency_field='company_currency_id')
+    
+class LogisflooPayment(models.Model):
+    _inherit = 'account.payment'
+
+    property_journal_paid_by3rd_pty_id = fields.Many2one(
+        'account.journal', 
+        company_dependent=True,
+        string="Paid by 3rd party Journal", 
+        domain="[('deprecated', '=', False)]",
+        help="This journal will be used to record the payment by third party",
+        required=True)
+            
+    @api.multi
+    def post(self):
+        _logger.info('Going through LogisflooPayment *************')
+        # if there is a third party payment, then we only process one invoice at a time (I've no time to do more)    
+        # if there is no third party payment, then we let it through the standard code       
         
-        
+        if len(self) > 1:
+            raise UserError(_("Only one payment can be processed at a time. Trying to post %s payments") % len(self))
+
+        if len(self.invoice_ids) > 1:
+            for inv in self.invoice_ids:
+                if inv.tpty_partner_id:  
+                    raise UserError(_("Only one invoice with third party payer can be processed at a time."))
+              
+        if self.invoice_ids[0].partner_id != self.invoice_ids[0].tpty_partner_id and self.invoice_ids[0].tpty_partner_id:
+            # Create the vendor entries in the paid by 3rd party journal
+            old_journal_id=self.journal_id
+            self.journal_id=self.property_journal_paid_by3rd_pty_id
+            super(LogisflooPayment,self).post()
+            self.journal_id=old_journal_id
+            
+            # Create the 3rd party entries in the request journal
+            vendor_id = self.invoice_ids[0].partner_id 
+            old_name=self.name
+            
+            self.partner_id = self.invoice_ids[0].tpty_partner_id
+            self.name = self.env['ir.sequence'].with_context(ir_sequence_date=self.payment_date).next_by_code('account.tptypayment.supplier.invoice')
+            amount = self.amount * (self.payment_type in ('outbound', 'transfer') and 1 or -1)
+            self._create_tpty_payment_entry(amount)
+            
+            self.partner_id = vendor_id
+            self.name=old_name
+        else:
+            super(LogisflooPayment,self).post()
+            
+    def _create_tpty_payment_entry(self, amount):
+        """ Create a journal entry corresponding to a payment, if the payment references invoice(s) they are reconciled.
+            Return the journal entry.
+        """
+        aml_obj = self.env['account.move.line'].with_context(check_move_validity=False)
+        invoice_currency = False
+        if self.invoice_ids and all([x.currency_id == self.invoice_ids[0].currency_id for x in self.invoice_ids]):
+            #if all the invoices selected share the same currency, record the paiement in that currency too
+            invoice_currency = self.invoice_ids[0].currency_id
+        debit, credit, amount_currency, currency_id = aml_obj.with_context(date=self.payment_date).compute_amount_fields(amount, self.currency_id, self.company_id.currency_id, invoice_currency)
+
+        move = self.env['account.move'].create(self._get_move_vals())
+
+        #Write line corresponding to invoice payment  
+        counterpart_aml_dict = self._get_shared_move_line_vals(debit, credit, amount_currency, move.id, False)
+        counterpart_aml_dict.update(self._get_tpty_counterpart_move_line_vals(self.invoice_ids))
+        counterpart_aml_dict.update({'currency_id': currency_id})
+        aml_obj.create(counterpart_aml_dict)
+
+        #Write counterpart lines
+        if not self.currency_id != self.company_id.currency_id:
+            amount_currency = 0
+        liquidity_aml_dict = self._get_shared_move_line_vals(credit, debit, -amount_currency, move.id, False)
+        liquidity_aml_dict.update(self._get_liquidity_move_line_vals(-amount))
+        aml_obj.create(liquidity_aml_dict)
+
+        move.post()
+        return move
+
+    def _get_tpty_counterpart_move_line_vals(self, invoice=False):
+        if self.payment_type == 'transfer':
+            name = self.name
+        else:
+            name = ''
+            if self.partner_type == 'customer':
+                if self.payment_type == 'inbound':
+                    name += _("Customer Payment")
+                elif self.payment_type == 'outbound':
+                    name += _("Customer Refund")
+            elif self.partner_type == 'supplier':
+                if self.payment_type == 'inbound':
+                    name += _("Vendor Refund")
+                elif self.payment_type == 'outbound':
+                    name += _("Third Party Payment")
+            if invoice:
+                name += ': '
+                for inv in invoice:
+                    if inv.move_id:
+                        name += inv.number + ', '
+                name = name[:len(name)-2] 
+        return {
+            'name': name,
+            'account_id': self.property_journal_paid_by3rd_pty_id.default_debit_account_id.id,
+            'journal_id': self.journal_id.id,
+            'currency_id': self.currency_id != self.company_id.currency_id and self.currency_id.id or False,
+            'payment_id': self.id,
+        }
