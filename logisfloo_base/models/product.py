d4ce0ce3d3bdded91732a1fb56fb04cc3940cdce
Guy Raspé <guy.raspe@gmail.com>
2018-09-09 16:17:45 +0200

    Automatically set product pos category based on internal category

--- logisfloo_base/models/product.py
+++ logisfloo_base/models/product.py
@@ -3,19 +3,29 @@ from openerp import models, fields, api
 from openerp.tools.translate import _
 from openerp.exceptions import UserError
 
+import logging
+_logger = logging.getLogger(__name__)
+
 class LogisflooProduct(models.Model):
     _inherit = 'product.template'
     
-#    total_with_margin = fields.Float(compute='_get_total_with_margin', store=False, string="Total Sales Price with Margin")
     total_with_margin = fields.Float(compute='_compute_cost', store=False, string="Total Sales Price with Margin")
     reference_price = fields.Float(string='Invoice Price')
     producer = fields.Char(string="Producer")
     default_code = fields.Char(store=True)
     internal_ref = fields.Char(string="Internal Reference")
+    pos_categ_id = fields.Many2one('pos.category','Point of Sale Category', 
+                                   help="Those categories are used to group similar products for point of sale.")
 
     _defaults = {
         'type' : 'product',
     }
+
+    @api.multi
+    @api.onchange('categ_id')
+    def _align_categories(self):
+        poscateg =self.env['pos.category'].search([('name', '=', self.categ_id.name)])[0]
+        self.pos_categ_id = poscateg.id
 
     @api.one
     def _get_total_with_margin(self):
@@ -38,6 +48,7 @@ class LogisflooProduct(models.Model):
                 total_taxes += currency.round(taxes_id._compute_amount(discounted_sell_unit_price, discounted_sell_unit_price)) 
             self.total_with_margin += total_taxes
             self.total_with_margin = self.total_with_margin * (1.05)
+
 
 class LogisflooProductCategory(models.Model):  
     _inherit = "product.category"
