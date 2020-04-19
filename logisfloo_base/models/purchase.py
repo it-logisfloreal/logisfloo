711701d9c9730d2f06eb530db56d4ee586661084
Guy Raspé <guy.raspe@gmail.com>
2020-04-19 21:56:41 +0200

    FIX: skip standard price update for non product.
    Invoice lines not corresponding to a product (e.g rounding) do not have
    standard price and no unit (division by zero error).

--- logisfloo_base/models/purchase.py
+++ logisfloo_base/models/purchase.py
@@ -272,21 +272,23 @@ class LogisflooAccountInvoice(models.Model):
         vendor_invoices=[x for x in self if x.type == 'in_invoice']
         for invoice in vendor_invoices:
             for line in self.env['account.invoice.line'].search([('invoice_id','=',invoice.id),('quantity', '>',0.0)]):
+                _logger.info('Validate product invoice line with description: %s', line.name)
                 product=self.env['product.product'].search([('id','=',line.product_id.id)])            
                 template=self.env['product.template'].search([('id','=',product.product_tmpl_id.id)])
-                currency = template.currency_id
-                tax_ratio = 0
-                suppliers = template._get_main_supplier_info()
-                if(len(suppliers) > 0):
-                    for taxes_id in template.supplier_taxes_id:
-                        tax_ratio += currency.round(taxes_id._compute_amount(1.0, 1.0)) 
-                    tax_ratio += 1
-                else:
-                    tax_ratio = 1
-                tax_unit_ratio = (template.uom_po_id.factor/template.uom_id.factor)*tax_ratio
-                invoice_unit_price=line.price_subtotal/line.quantity * tax_unit_ratio
-                if invoice_unit_price != product.standard_price:
-                    product.standard_price=invoice_unit_price        
+                if len(template)>0:     
+                    currency = template.currency_id
+                    tax_ratio = 0
+                    suppliers = template._get_main_supplier_info()
+                    if(len(suppliers) > 0):
+                        for taxes_id in template.supplier_taxes_id:
+                            tax_ratio += currency.round(taxes_id._compute_amount(1.0, 1.0)) 
+                        tax_ratio += 1
+                    else:
+                        tax_ratio = 1
+                    tax_unit_ratio = (template.uom_po_id.factor/template.uom_id.factor)*tax_ratio
+                    invoice_unit_price=line.price_subtotal/line.quantity * tax_unit_ratio
+                    if invoice_unit_price != product.standard_price:
+                        product.standard_price=invoice_unit_price        
         return data
  
 class LogisflooCalcAdjustWizard(models.TransientModel):
